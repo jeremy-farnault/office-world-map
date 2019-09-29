dc8cb08efa815ddc0d00a9c94aaf62f70264a2d3
Jeremy Farnault <haskkor@gmail.com>
2019-09-29 17:51:21 +1300

    Adding ts etc

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { withAuthenticator } from "aws-amplify-react";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+};
