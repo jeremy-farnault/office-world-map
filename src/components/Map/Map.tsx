f837ac10cd758b8f68eada0409408a35e565226f
Jeremy Farnault <haskkor@gmail.com>
2019-10-10 20:13:36 +1300

    Added the map library

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,5 +1,10 @@
+import { GoogleApiWrapper } from 'google-maps-react'
 import React, { memo } from 'react'
 
 export const Map: React.FC = memo(() => {
   return <div></div>
 })
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
+})(Map)
