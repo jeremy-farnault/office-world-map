46ee9a5112572febd8f07afc84208c6b01010c76
Jeremy Farnault <haskkor@gmail.com>
2019-10-13 18:46:31 +1300

    Add default position to 171

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -2,7 +2,16 @@ import { GoogleApiWrapper, Map } from 'google-maps-react'
 import React, { memo } from 'react'
 
 const MapContainer: React.FC = memo(() => {
-  return <Map google={window.google} zoom={14} />
+  return (
+    <Map
+      google={window.google}
+      initialCenter={{
+        lat: -41.284378,
+        lng: 174.776847
+      }}
+      zoom={14}
+    />
+  )
 })
 
 export const GoogleMap = GoogleApiWrapper({
