a30ec30b2045218ae5bdc2bcc153b139e22995c3
Jeremy Farnault <haskkor@gmail.com>
2019-11-02 20:13:52 +1300

    Things

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
 // @ts-ignore
 import { withAuthenticator } from 'aws-amplify-react'
-import { Auth } from 'aws-amplify'
+import { API, Auth, graphqlOperation } from 'aws-amplify'
 import React, { useEffect } from 'react'
 import './App.css'
 import { Filters, GoogleMap } from './components'
+import { getEmployeeDetails } from './graphql/queries'
 
 const App: React.FC = () => {
   useEffect(() => {
     Auth.currentAuthenticatedUser()
-      .then(user => console.log({ user }))
+      .then(user => {
+        getData()
+        console.log({ user })
+      })
       .catch(error => console.log({ error }))
   })
 
+  async function getData() {
+    try {
+      const coinData = await API.graphql(graphqlOperation(getEmployeeDetails))
+      console.log('data from API: ', coinData)
+    } catch (err) {
+      console.log('error fetching data..', err)
+    }
+  }
+
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
       <Filters />
       <GoogleMap />
     </div>
--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,5 +1,9 @@
 import React, { memo } from 'react'
 
 export const Filters: React.FC = memo(() => {
-  return <div></div>
+  return (
+    <div>
+      <div style={{ height: 30, width: 50, color: 'black', border: '1px solid grey' }}>Add</div>
+    </div>
+  )
 })
